1b254ff1a40147e3d2acfdc4efc593475b785b62
ivana <ivananl@yahoo.com>
2021-05-20 15:06:41 +0200

    Add self join

--- Self join.sql
+++ Self join.sql
@@ -23,4 +23,13 @@ SELECT a.company, a.num, c.name, d.name FROM route a JOIN route b ON a.company =
 SELECT a.company, a.num FROM route a JOIN route b ON a.company = b.company AND a.num =b.num JOIN stops c ON a.stop = c.id JOIN stops d ON b.stop = d.id WHERE c.name = 'Haymarket'  AND d.name = 'Leith' group by a.company, a.num
 8.
 SELECT a.company, a.num FROM route a JOIN route b ON a.company = b.company AND a.num =b.num JOIN stops c ON a.stop = c.id JOIN stops d ON b.stop = d.id  WHERE c.name = 'Craiglockhart' AND d.name = 'Tollcross' GROUP BY a.company, a.num
+9.
+SELECT d.name, a.company, b.num FROM route a JOIN route b ON a.company = b.company AND a.num =b.num JOIN stops c ON a.stop = c.id JOIN stops d ON b.stop = d.id  WHERE c.name = 'Craiglockhart' AND a.company =  'LRT'
+10.
+SELECT r1.num, r1.company, s1.name, r4.num, r4.company 
+FROM route r1 JOIN route r2 ON  r1.num = r2.num AND r1.company = r2.company
+JOIN stops s1 ON r2.stop = s1.id
+JOIN route r3 ON s1.id = r3.stop
+JOIN route r4 ON r3.num = r4.num AND r3.company = r4.company
+WHERE r1.stop = (SELECT id FROM stops f WHERE f.name = 'Craiglockhart') and r4.stop = (SELECT id FROM stops e WHERE e.name = 'Lochend') ORDER BY r1.num, s1.name, r2.num
 
